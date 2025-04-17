efa79228c695a3c65d2271ce18f9d737139403bb
CantanTam <cantantam@gmail.com>
2025-04-18 01:02:15 +0800

    初步实现极速菜单唤出功能

--- call_popup_quick_menus.py
+++ call_popup_quick_menus.py
@@ -9,4 +9,6 @@ class CALLOUT_QUICK_MENU_OT_one(bpy.types.Operator):
         bpy.ops.popup.quick_menu_one('INVOKE_DEFAULT')
         return {'FINISHED'}
     
-# 因为 operators.py 当中 限过了快捷键功能的运行方式是 exec(f'bpy.ops.{op}()') ，无法在函数内部添加参数，而 bpy.ops.popup.quick_menu_one() 是无法调出自定义菜单的，所以需要再构建一个函数，调用 bpy.ops.popup.quick_menu_one('INVOKE_DEFAULT')
+# 因为 operators.py 当中 限过了快捷键功能的运行方式是 exec(f'bpy.ops.{op}()') 
+# ，无法在函数内部添加参数，而 bpy.ops.popup.quick_menu_one() 是无法调出自定义菜单的
+# ，所以需要再构建一个函数，调用 bpy.ops.popup.quick_menu_one('INVOKE_DEFAULT')
