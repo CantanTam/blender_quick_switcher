a462005b36a7aed5acc6dc98ef3ea76f6a71eb87
CantanTam <cantantam@gmail.com>
2025-06-11 11:56:14 +0800

    armature edit功能添加完成

--- quick_menu_button_functions/button_actions_armature_edit.py
+++ quick_menu_button_functions/button_actions_armature_edit.py
@@ -1,8 +1,6 @@
 import bpy
 
-# 以下是“物体”模式当中“物体”菜单特有的一些选项
-
-# “选择”菜单——选择相连
+# “骨架”菜单
 class BUTTON_ACTION_OT_armatureedit_select_select_linked(bpy.types.Operator):
     bl_idname = "button.action_armatureedit_select_select_linked"
     bl_label = "选择相连"
@@ -87,6 +85,336 @@ class BUTTON_ACTION_OT_armatureedit_select_similar(bpy.types.Operator):
         bpy.ops.armature.select_similar(type=self.type, threshold=self.threshold)
         return {'FINISHED'}
 
+class BUTTON_ACTION_OT_armatureedit_align(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_align"
+    bl_label = "对齐骨骼"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.armature.align()
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_armature_roll_menu(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_armature_roll_menu"
+    bl_label = "骨骼扭转"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.wm.call_menu(name="VIEW3D_MT_edit_armature_roll")
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_armature_calculate_roll_menu(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_armature_calculate_roll_menu"
+    bl_label = "重算扭转"
+    bl_description = "快捷键 Shift N"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.armature.calculate_roll('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_transform_bone_roll(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_transform_bone_roll"
+    bl_label = "设置扭转"
+    bl_description = "快捷键 Ctrl R"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.transform.transform('INVOKE_DEFAULT',mode='BONE_ROLL')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_roll_clear(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_roll_clear"
+    bl_label = "清除扭转"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    roll: bpy.props.FloatProperty(
+        default=0,
+        min=-6.28319,
+        max=6.28319,
+        subtype='ANGLE',
+    )
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def draw(self, context):
+        layout = self.layout
+        split = layout.row().split(factor=0.4)
+        
+        col_left = split.column()
+        col_left.alignment = 'RIGHT'
+        col_right = split.column()
+
+        col_left.label(text="扭转(滚动)")
+        col_right.prop(self, "roll", text="")
+
+    def execute(self, context):    
+        bpy.ops.armature.roll_clear(roll=self.roll)
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_extrude_move(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_extrude_move"
+    bl_label = "挤出"
+    bl_description = "快捷键 E"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.extrude_move('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_fill(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_fill"
+    bl_label = "在关节间填充骨骼"
+    bl_description = "快捷键 F"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        selected_heads = [b for b in context.active_object.data.edit_bones if b.select_head]
+        selected_tails = [b for b in context.active_object.data.edit_bones if b.select_tail]
+        
+        if not (selected_heads or selected_tails):
+            return False
+        
+        head_bones = {b for b in selected_heads}
+        tail_bones = {b for b in selected_tails}
+        
+        if head_bones & tail_bones:
+            return False
+        
+        if len(selected_heads) + len(selected_tails) > 2:
+            return False
+        
+        return True
+
+    def execute(self, context):    
+        bpy.ops.armature.fill()
+        return {'FINISHED'}
+    
+class BUTTON_ACTION_OT_armatureedit_split(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_split"
+    bl_label = "拆分"
+    bl_description = "快捷键 Y"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.split()
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_separate(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_separate"
+    bl_label = "分离骨骼"
+    bl_description = "快捷键 P"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.separate('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_subdivide(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_subdivide"
+    bl_label = "细分"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    number_cuts: bpy.props.IntProperty(
+        default=1,
+        min=1,
+        max=1000,
+        soft_max=10,
+    )
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def draw(self, context):
+        layout = self.layout
+        split = layout.row().split(factor=0.4)
+        
+        col_left = split.column()
+        col_left.alignment = 'RIGHT'
+        col_right = split.column()
+
+        col_left.label(text="切割次数")
+        col_right.prop(self, "number_cuts", text="")
+
+    def execute(self, context):    
+        bpy.ops.armature.subdivide(number_cuts=self.number_cuts)
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_switch_direction(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_switch_direction"
+    bl_label = "切换方向"
+    bl_description = "快捷键 Alt F"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.switch_direction()
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_symmetrize(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_symmetrize"
+    bl_label = "对称"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    direction: bpy.props.EnumProperty(
+        items=[
+            ('NEGATIVE_X', "-X 到 +X", ""),
+            ('POSITIVE_X', "+X 到 -X", ""),
+        ],
+        default='NEGATIVE_X'
+    )
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def draw(self, context):
+        layout = self.layout
+        split = layout.row().split(factor=0.4)
+        
+        col_left = split.column()
+        col_left.alignment = 'RIGHT'
+        col_right = split.column()
+
+        col_left.label(text="方向")
+        col_right.prop(self, "direction", text="abc", expand=True)
+
+    def execute(self, context):    
+        bpy.ops.armature.symmetrize(direction=self.direction)
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_name_menu(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_name_menu"
+    bl_label = "名称"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.wm.call_menu(name="VIEW3D_MT_edit_armature_names")
+        return {'FINISHED'}
+    
+class BUTTON_ACTION_OT_armatureedit_armature_layers(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_armature_layers"
+    bl_label = "更改骨架层"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.armature_layers('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_bone_layers(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_bone_layers"
+    bl_label = "更改骨骼层"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.bone_layers('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_edit_armature_parent(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_edit_armature_parent"
+    bl_label = "父级"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.wm.call_menu(name="VIEW3D_MT_edit_armature_parent")
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_parent_set(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_parent_set"
+    bl_label = "生成父级"
+    bl_description = "快捷键 Ctrl P"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.parent_set('INVOKE_DEFAULT')
+        return {'FINISHED'}
+    
+class BUTTON_ACTION_OT_armatureedit_parent_clear(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_parent_clear"
+    bl_label = "清空父级"
+    bl_description = "快捷键 Alt P"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.parent_clear('INVOKE_DEFAULT')
+        return {'FINISHED'}
+    
+class BUTTON_ACTION_OT_armatureedit_bone_options_toggle(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_bone_options_toggle"
+    bl_label = "骨骼设置"
+    bl_description = "快捷键 Shift W"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.wm.call_menu(name="VIEW3D_MT_bone_options_toggle")
+        return {'FINISHED'}
+# 4.3版本以后的功能
+class BUTTON_ACTION_OT_armatureedit_bone_collections(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_bone_collections"
+    bl_label = "骨骼集合"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.wm.call_menu(name="VIEW3D_MT_bone_collections")
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_move_to_collection(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_move_to_collection"
+    bl_label = "移动到集合"
+    bl_description = "快捷键 M"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.move_to_collection('INVOKE_DEFAULT')
+        return {'FINISHED'}
+    
+class BUTTON_ACTION_OT_armatureedit_assign_to_collection(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_assign_to_collection"
+    bl_label = "指定到集合"
+    bl_description = "快捷键 Shift M"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.assign_to_collection('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_collection_show_all(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_collection_show_all"
+    bl_label = "显示全部"
+    bl_description = "快捷键 Ctrl `"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):    
+        bpy.ops.armature.collection_show_all()
+        return {'FINISHED'}
+
+class BUTTON_ACTION_OT_armatureedit_collection_create_and_assign(bpy.types.Operator):
+    bl_idname = "button.action_armatureedit_collection_create_and_assign"
+    bl_label = "添加选中的骨骼到新集合"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    name: bpy.props.StringProperty(
+        default='New Collection',
+    )
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def draw(self, context):
+        layout = self.layout
+        split = layout.row().split(factor=0.4)
+        
+        col_left = split.column()
+        col_left.alignment = 'RIGHT'
+        col_right = split.column()
+
+        col_left.label(text="骨骼集合")
+        col_right.prop(self, "name", text="")
+
+    def execute(self, context):    
+        bpy.ops.armature.collection_create_and_assign(name=self.name)
+        return {'FINISHED'}
+
 
 
 
@@ -94,6 +422,31 @@ class BUTTON_ACTION_OT_armatureedit_select_similar(bpy.types.Operator):
 classes = (
     BUTTON_ACTION_OT_armatureedit_select_select_linked,
     BUTTON_ACTION_OT_armatureedit_select_similar,
+    BUTTON_ACTION_OT_armatureedit_align,
+    BUTTON_ACTION_OT_armatureedit_armature_roll_menu,
+    BUTTON_ACTION_OT_armatureedit_armature_calculate_roll_menu,
+    BUTTON_ACTION_OT_armatureedit_transform_bone_roll,
+    BUTTON_ACTION_OT_armatureedit_roll_clear,
+    BUTTON_ACTION_OT_armatureedit_extrude_move,
+    BUTTON_ACTION_OT_armatureedit_fill,
+    BUTTON_ACTION_OT_armatureedit_split,
+    BUTTON_ACTION_OT_armatureedit_separate,
+    BUTTON_ACTION_OT_armatureedit_subdivide,
+    BUTTON_ACTION_OT_armatureedit_switch_direction,
+    BUTTON_ACTION_OT_armatureedit_symmetrize,
+    BUTTON_ACTION_OT_armatureedit_name_menu,
+    BUTTON_ACTION_OT_armatureedit_armature_layers,
+    BUTTON_ACTION_OT_armatureedit_bone_layers,
+    BUTTON_ACTION_OT_armatureedit_edit_armature_parent,
+    BUTTON_ACTION_OT_armatureedit_parent_set,
+    BUTTON_ACTION_OT_armatureedit_parent_clear,
+    BUTTON_ACTION_OT_armatureedit_bone_options_toggle,
+
+    BUTTON_ACTION_OT_armatureedit_bone_collections,
+    BUTTON_ACTION_OT_armatureedit_move_to_collection,
+    BUTTON_ACTION_OT_armatureedit_assign_to_collection,
+    BUTTON_ACTION_OT_armatureedit_collection_show_all,
+    BUTTON_ACTION_OT_armatureedit_collection_create_and_assign,
 
 )
 
--- right_click_to_switcher/right_click_add_armature_edit.py
+++ right_click_to_switcher/right_click_add_armature_edit.py
@@ -23,12 +23,140 @@ def draw_add_to_switcher_armature_edit(self, context):
         layout.separator()
         layout.operator("call.add_to_switcher_menu", text="\"选择相似(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_select_similar'
 
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_align":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"对齐骨骼\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_align'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_extrude_move":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"挤出\"⟶Switcher", icon='EVENT_E').action = 'button.action_armatureedit_extrude_move'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_fill":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"在关节间填充骨骼\"⟶Switcher", icon='EVENT_F').action = 'button.action_armatureedit_fill'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_split":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"拆分\"⟶Switcher", icon='EVENT_Y').action = 'button.action_armatureedit_split'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_separate":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"分离骨骼\"⟶Switcher", icon='EVENT_P').action = 'button.action_armatureedit_separate'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_subdivide":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"细分\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_subdivide'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_switch_direction":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"切换方向\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_switch_direction'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_symmetrize":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"对称\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_symmetrize'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_armature_layers":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"更改骨架层\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_armature_layers'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_bone_layers":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"更改骨骼层\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_bone_layers'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_parent_set":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"生成父级\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_parent_set'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_parent_clear":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"清空父级\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_parent_clear'
+    # 4.3 版本有效
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_move_to_collection":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"移动到集合\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_move_to_collection'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_assign_to_collection":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"指定到集合\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_assign_to_collection'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_collection_show_all":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"显示全部\"⟶Switcher", icon='HIDE_OFF').action = 'button.action_armatureedit_collection_show_all'
+
+    elif op and op.bl_rna.identifier == "ARMATURE_OT_collection_create_and_assign":
+        layout = self.layout
+        layout.separator()
+        layout.operator("call.add_to_switcher_menu", text="\"指定到新集合\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_collection_create_and_assign'
+
+
+def armatureedit_armature_roll_menu_to_switcher(self, context):
+    show_switcher = bpy.context.preferences.addons[ADDON_NAME].preferences.to_show_to_switcher
+    if not show_switcher:
+        return
+    self.layout.separator()
+    self.layout.operator("call.add_to_switcher_menu", text="\"骨骼扭转(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_armature_roll_menu'
+    self.layout.operator("call.add_to_switcher_menu", text="\"重算扭转\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_armature_calculate_roll_menu'
+    self.layout.operator("call.add_to_switcher_menu", text="\"设置扭转\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_transform_bone_roll'
+    self.layout.operator("call.add_to_switcher_menu", text="\"清除扭转\"⟶Switcher", icon='PLUS').action = 'button.action_armatureedit_roll_clear'
+
+def armatureedit_name_roll_menu_to_switcher(self, context):
+    show_switcher = bpy.context.preferences.addons[ADDON_NAME].preferences.to_show_to_switcher
+    if not show_switcher:
+        return
+    self.layout.separator()
+    self.layout.operator("call.add_to_switcher_menu", text="\"名称(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_name_menu'
+
+def armatureedit_armature_parent_menu_to_switcher(self, context):
+    show_switcher = bpy.context.preferences.addons[ADDON_NAME].preferences.to_show_to_switcher
+    if not show_switcher:
+        return
+    self.layout.separator()
+    self.layout.operator("call.add_to_switcher_menu", text="\"父级(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_edit_armature_parent'
+
+def armatureedit_bone_options_toggle_menu_to_switcher(self, context):
+    show_switcher = bpy.context.preferences.addons[ADDON_NAME].preferences.to_show_to_switcher
+    if not show_switcher:
+        return
+    self.layout.separator()
+    self.layout.operator("call.add_to_switcher_menu", text="\"骨骼设置(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_bone_options_toggle'
+
+def armatureedit_bone_collections_menu_to_switcher(self, context):
+    show_switcher = bpy.context.preferences.addons[ADDON_NAME].preferences.to_show_to_switcher
+    if not show_switcher:
+        return
+    self.layout.separator()
+    self.layout.operator("call.add_to_switcher_menu", text="\"骨骼集合(菜单)\"⟶Switcher", icon='PRESET').action = 'button.action_armatureedit_bone_collections'
 
 
 
 
 def register():
     bpy.types.UI_MT_button_context_menu.append(draw_add_to_switcher_armature_edit)
+    bpy.types.VIEW3D_MT_edit_armature_roll.append(armatureedit_armature_roll_menu_to_switcher)
+    bpy.types.VIEW3D_MT_edit_armature_names.append(armatureedit_name_roll_menu_to_switcher)
+    bpy.types.VIEW3D_MT_edit_armature_parent.append(armatureedit_armature_parent_menu_to_switcher)
+    bpy.types.VIEW3D_MT_bone_options_toggle.append(armatureedit_bone_options_toggle_menu_to_switcher)
+    bpy.types.VIEW3D_MT_bone_collections.append(armatureedit_bone_collections_menu_to_switcher)
 
 def unregister():
+    bpy.types.VIEW3D_MT_bone_collections.remove(armatureedit_bone_collections_menu_to_switcher)
+    bpy.types.VIEW3D_MT_bone_options_toggle.remove(armatureedit_bone_options_toggle_menu_to_switcher)
+    bpy.types.VIEW3D_MT_edit_armature_parent.remove(armatureedit_armature_parent_menu_to_switcher)
+    bpy.types.VIEW3D_MT_edit_armature_names.remove(armatureedit_name_roll_menu_to_switcher)
+    bpy.types.VIEW3D_MT_edit_armature_roll.remove(armatureedit_armature_roll_menu_to_switcher)
     bpy.types.UI_MT_button_context_menu.remove(draw_add_to_switcher_armature_edit)
